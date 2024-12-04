7592f9bfc95f62b1b82c229e1931a4273e7f5876
menirenshtein <m4584a@gmail.com>
2024-12-04 14:44:05 +0200

    add all files

---
+++ clock/src/App.tsx
@@ -0,0 +1,11 @@
+import Clock from "./pages/Clock"
+import './App.css'
+const App = () => {
+  return (
+    <div className="app">
+      <Clock/>
+    </div>
+  )
+}
+
+export default App
---
+++ clock/src/pages/Clock.tsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import './clock.css';
+
+const CountdownTimer: React.FC = () => {
+  const targetDate = new Date(2025, 0, 2, 18, 0, 0); // 2 בינואר 2025
+
+  const [timeLeft, setTimeLeft] = useState<string>('');
+
+  useEffect(() => {
+    const updateCountdown = () => {
+      const now = new Date();
+      const difference = targetDate.getTime() - now.getTime();
+
+      if (difference <= 0) {
+        setTimeLeft('הזמן הגיע!');
+        return;
+      }
+
+      const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((difference / (1000 * 60)) % 60);
+      const seconds = Math.floor((difference / 1000) % 60);
+
+      setTimeLeft(
+        `${days} : ${hours} : ${minutes} : ${seconds} `
+      );
+    };
+
+    updateCountdown(); 
+    const intervalId = setInterval(updateCountdown, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="countdown-timer">
+      <div className="drip drip-small"></div>
+      <div className="drip drip-medium"></div>
+      <div className="drip drip-large"></div>
+      <div className='noteDiv'>
+      <p className='note'> עד מתי קורס פולסטאק 2025 </p>
+
+      </div>
+      {timeLeft}
+      <div className='nota'>
+
+      <p className='days'>ימים</p>
+      <p className='days'>  שעות</p>
+      <p className='days'> דקות</p>
+      <p className='days'>  שניות </p>
+      </div>
+    </div>
+  );
+};
+
+export default CountdownTimer;
